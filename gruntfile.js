a6a194d4437cce9524e06eb9605ab2d214df4d65
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-07 15:09:55 +0100

    added some less-css and updated the grunt file to use the linter

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,18 @@
 (function() {
     'use strict';
-    var copyTask = require('./.grunt/copy-task');
+    var copyTask = require('./.grunt/copy-task'),
+        lessTask = require('./.grunt/less-task');
     module.exports = function (grunt) {
         grunt.initConfig({
             pkg: grunt.file.readJSON('package.json'),
-            copy: copyTask
+            copy: copyTask,
+            lesslint: lessTask,
+            less: lessTask
         });
         grunt.loadNpmTasks('grunt-contrib-copy');
-        grunt.registerTask('default', ['copy']);
+        grunt.loadNpmTasks('grunt-lesslint');
+        grunt.loadNpmTasks('grunt-contrib-less');
+        grunt.registerTask('lessFiles', ['lesslint', 'less']);
+        grunt.registerTask('default', ['copy', lessFiles]);
     };
 })();
