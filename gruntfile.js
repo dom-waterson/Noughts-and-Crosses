c27a5f96f089e3adc585bc8787e39aeead1833d2
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-07 14:50:05 +0100

    refactored grunt task files

--- gruntfile.js
+++ gruntfile.js
@@ -1,34 +1,10 @@
 (function() {
     'use strict';
+    var copyTask = require('./.grunt/copy-task');
     module.exports = function (grunt) {
         grunt.initConfig({
             pkg: grunt.file.readJSON('package.json'),
-            copy: {
-                mainappimages: {
-                    cwd:'',
-                    src: ['main-app/app/images/**/*.*'],
-                    dest: '.build/',
-                    expand: true
-                },
-                mainappsounds: {
-                    cwd:'',
-                    src: ['main-app/app/sounds/*.*'],
-                    dest: '.build/',
-                    expand: true
-                },
-                mainapphtml: {
-                    cwd:'',
-                    src: ['main-app/app/*.*'],
-                    dest: '.build/',
-                    expand: true
-                },
-                mainappbower: {
-                    cwd:'',
-                    src: ['bower_components/**/*.*'],
-                    dest: '.build/thirdparty/',
-                    expand: true
-                }
-            }
+            copy: copyTask
         });
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.registerTask('default', ['copy']);
