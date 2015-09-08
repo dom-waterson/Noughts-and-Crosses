8f2fa2ba4a66b29d720cf6c11eaec1e2ecd89cc3
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-08 14:32:31 +0100

    updated concat subtask to get any js file

--- .grunt/js-concat.js
+++ .grunt/js-concat.js
@@ -2,10 +2,8 @@
     'use strict';
     module.exports = {
         concatjs: {
-
-            src: ['main-app/app/scripts/temp.js'],
+            src: ['main-app/app/scripts/*.js'],
             dest: '.build/main-app/app/scripts/app.js'
         }
-
     };
 })();
