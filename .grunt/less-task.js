7fe387507a239dc6b7ba1bb26e7deadf0392e5dd
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-08 16:01:25 +0100

    refactored less grunt task

--- .grunt/less-task.js
+++ .grunt/less-task.js
@@ -4,7 +4,7 @@
         convertless: {
             expand: true,
             cwd: 'main-app/app/less/',
-            src: '**/*.less',
+            src: '*.less',
             dest: '.build/main-app/app/css',
             ext: '.css'
         }
