d526b680817d8c2f2aee088af512a7c1a68b0259
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-09 17:29:43 +0100

    add code to change the game board string

--- main-app/app/scripts/temp.js
+++ main-app/app/scripts/temp.js
@@ -2,8 +2,40 @@
     'use strict';
     angular.module('Tombola.NoughtAndCrosses.Core', [])
         .controller('MainController', function ($scope){
-            $scope.player1 = '';
+            var me = this;
+            var currentPlayer = '1';
+            $scope.player1 = 'test';
             $scope.player2 = '';
             $scope.gameboard = '000000000';
+
+            me.setCharAt = function (str, index, character) {
+                if(index > str.length-1) return str;
+                return str.substr(0,index) + character + str.substr(index+1);
+            };
+
+            $scope.gameboardTapped = function (gridNumberFromTable) {
+                if (currentPlayer === '1') {
+                    //alert(gridNumberFromTable + "player: " + currentPlayer);
+                    //var temp = $scope.gameboard;
+                    //$scope.gameboard = setCharAt(temp, gridNumberFromTable, currentPlayer);
+                    //alert($scope.gameboard);
+                    var str = 'Hello World';
+                    //str = setCharAt(str,4,',');
+                    alert(str);
+                    currentPlayer = '2';
+                }
+                else {
+                    //alert(gridNumberFromTable + "player: " + currentPlayer);
+                    //var temp = $scope.gameboard;
+                    //$scope.gameboard = setCharAt(temp, gridNumberFromTable, currentPlayer);
+                    //alert($scope.gameboard);
+                    var str = 'Hello World';
+                    str = setCharAt(str,4,',');
+                    alert(str);
+                    currentPlayer = '1';
+                }
+            };
+
+
         });
 })();
