5c7fbb5141915136985b28ee99790ab7e9c6fc74
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-10 12:24:38 +0100

    got the game board UI working and added some css to make the UI look nicer

--- main-app/app/scripts/temp.js
+++ main-app/app/scripts/temp.js
@@ -6,7 +6,6 @@
             $scope.player1 = 'human';
             $scope.player2 = 'human';
             $scope.gameboard = '000000000';
-            $scope.imageURL = '1.png';
 
             var setCharAt = function (string, index, character) {
                 if(index > string.length-1) {
@@ -15,21 +14,29 @@
                 return string.substr(0,index) + character + string.substr(index+1);
             };
 
-            var setImageOnBoard = function (playerNumber) {
-
+            var setImageOnBoard = function (idNumber) {
+                var index = idNumber.toString();
+                var tableCell = document.getElementById(index);
+                var img = document.createElement("img");
+                img.src = "images/" + currentPlayer + ".png";
+                tableCell.appendChild(img);
             };
 
             $scope.gameboardTapped = function (gridNumberFromTable) {
                 if (currentPlayer === '1') {
                     $scope.gameboard = setCharAt($scope.gameboard, gridNumberFromTable, currentPlayer);
-                    setImageOnBoard(currentPlayer);
+                    setImageOnBoard(gridNumberFromTable);
                     currentPlayer = '2';
                 }
                 else {
                     $scope.gameboard = setCharAt($scope.gameboard, gridNumberFromTable, currentPlayer);
-                    setImageOnBoard(currentPlayer);
+                    setImageOnBoard(gridNumberFromTable);
                     currentPlayer = '1';
                 }
+            };
+
+            var checkGrid = function (gridToCheck) {
+
             };
 
 
