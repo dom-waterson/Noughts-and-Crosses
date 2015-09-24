502aeebce3288f00a5eb403614f4021000fb7be2
Dominic Waterson <dominic.waterson@tombola.com>
2015-09-24 15:51:27 +0100

    added directive for game board, updated less and game status directive wasn't using new service game model

---
+++ main-app/app/scripts/Gameboard/directives/gameboardCells.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+    angular.module('Tombola.NoughtAndCrosses.gameboard')
+        .directive('gameCell', function (){
+            return {
+                restrict: 'E',
+                template: function (attr, element) {
+                   return '<div ng-click="game.gameboardTapped('+element.squareNumber+')" class= "cellSize player{{game.gameModel.gameboard['+element.squareNumber+']}}"></div>';
+                }
+            };
+        });
+})();
