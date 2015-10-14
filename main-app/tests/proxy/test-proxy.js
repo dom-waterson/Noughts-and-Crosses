849e8246ff61f3f298b5045f7492f6063f5c9ebf
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-14 12:06:23 +0100

    updated proxy test

--- main-app/tests/proxy/test-proxy.js
+++ main-app/tests/proxy/test-proxy.js
@@ -7,36 +7,35 @@
             module('Tombola.NoughtAndCrosses.core');
             inject(function ($injector) {
                 constants = $injector.get('Proxy');
-                $httpBackend = $injector.get('$httpBackend')
+                $httpBackend = $injector.get('$httpBackend');
             });
         });
 
         it('Ensures makeGame function creates a new game successfully from api', function () {
-            var theResponse = {'outcome':'Continue','gameboard':'100000000','winner':0};
             $httpBackend.expectPOST('http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame', {'player1': 'human',
-                'player2': 'human'}).respond(theResponse);
+                'player2': 'human'}).respond({'outcome':'Continue','gameboard':'100000000','winner':0});
 
             var returnedPromise = constants.makeGame("human", "human");
-            var result;
             returnedPromise.then(function(response){
-                result = response;
+                response.should.deep.equal({'outcome':'Continue','gameboard':'100000000','winner':0});
             });
             $httpBackend.flush();
-            result.should.deep.equal(theResponse);
         });
 
         it('Ensures makeMove function successfully communicates with the api', function () {
-            var theResponse = {'outcome':'Continue','gameboard':'100000000','winner':0};
             $httpBackend.expectPOST('http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove', {'playerNumber': '1',
-                'chosenSquare': '0'}).respond(theResponse);
+                'chosenSquare': '0'}).respond({'outcome':'Continue','gameboard':'100000000','winner':0});
 
             var returnedPromise = constants.makeGameMove("1", "0");
-            var result;
             returnedPromise.then(function(response){
-                result = response;
+                response.should.deep.equal({'outcome':'Continue','gameboard':'100000000','winner':0});
             });
             $httpBackend.flush();
-            result.should.deep.equal(theResponse);
+        });
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
         });
     });
 })();
