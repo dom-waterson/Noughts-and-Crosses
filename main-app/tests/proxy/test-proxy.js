e2c28d73fb0b76971e02704b6d9e41f944ff2230
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-14 15:41:10 +0100

    add variable to hold the response

--- main-app/tests/proxy/test-proxy.js
+++ main-app/tests/proxy/test-proxy.js
@@ -3,6 +3,7 @@
     describe('Testing proxy', function () {
         var constants,
             $httpBackend;
+        var responseData = {'outcome':'Continue','gameboard':'100000000','winner':0};
         beforeEach(function () {
             module('Tombola.NoughtAndCrosses.core');
             inject(function ($injector) {
@@ -13,22 +14,22 @@
 
         it('Ensures makeGame function creates a new game successfully from api', function () {
             $httpBackend.expectPOST('http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame', {'player1': 'human',
-                'player2': 'human'}).respond({'outcome':'Continue','gameboard':'100000000','winner':0});
+                'player2': 'human'}).respond(responseData);
 
             var returnedPromise = constants.makeGame("human", "human");
             returnedPromise.then(function(response){
-                response.should.deep.equal({'outcome':'Continue','gameboard':'100000000','winner':0});
+                response.should.deep.equal(responseData);
             });
             $httpBackend.flush();
         });
 
         it('Ensures makeMove function successfully communicates with the api', function () {
             $httpBackend.expectPOST('http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove', {'playerNumber': '1',
-                'chosenSquare': '0'}).respond({'outcome':'Continue','gameboard':'100000000','winner':0});
+                'chosenSquare': '0'}).respond(responseData);
 
             var returnedPromise = constants.makeGameMove("1", "0");
             returnedPromise.then(function(response){
-                response.should.deep.equal({'outcome':'Continue','gameboard':'100000000','winner':0});
+                response.should.deep.equal(responseData);
             });
             $httpBackend.flush();
         });
