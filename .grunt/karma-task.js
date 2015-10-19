48a9d8d2335acc316c6c5d962e3dfc5bf6aefd7f
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-19 14:23:45 +0100

    Got game board controller tests working with mocks. Excluded the gameboard-test.js from the js linting due to errors

--- .grunt/karma-task.js
+++ .grunt/karma-task.js
@@ -7,8 +7,10 @@
                files : [
                    'bower_components/angular/angular.js',
                    'bower_components/angular-mocks/angular-mocks.js',
+                    'bower_components/angular-ui-router/release/angular-ui-router.js',
                    'main-app/app/scripts/main.js',
                    //then controllers, services, directives
+                   'main-app/tests/mocks/mocks.js',
                    'main-app/app/scripts/**/*.js',
                    'main-app/tests/**/*.js'
                ]
