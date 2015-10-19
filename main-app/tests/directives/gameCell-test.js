4187b97edcc260673c3ee98f768454684922c2e7
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-19 17:17:34 +0100

    started testing game cell directive, got the ng-click left to test on it

---
+++ main-app/tests/directives/gameCell-test.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+    describe('Testing game cell directive', function() {
+        var compile,
+            rootScope,
+            sandbox;
+
+        beforeEach(function () {
+            module('Tombola.NoughtAndCrosses.gameboard');
+
+            inject(function ($compile, $rootScope) {
+                compile = $compile;
+                rootScope = $rootScope;
+            });
+
+            sandbox = sinon.sandbox.create();
+        });
+
+        it('Ensures game-cell directives works', function () {
+            var directiveElement =  '<game-cell square-number="4"></game-cell>';
+            var element = compile(directiveElement)(rootScope);
+            rootScope.$digest();
+            element[0].toString().should.be.equal('[object HTMLElement]');
+            element.attr('square-number').should.be.equal('4');
+            element[0].children.length.should.be.equal(1);
+
+            element.triggerHandler('click');
+            //assert(something);
+        });
+
+        afterEach(function () {
+            sinon.sandbox.restore();
+        });
+    });
+})();
