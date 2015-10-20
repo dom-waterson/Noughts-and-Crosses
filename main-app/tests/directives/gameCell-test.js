f7dda9b1263d2f06a0c188fc4a8e4322967d70fd
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-20 16:40:38 +0100

    first test for directive complete

--- main-app/tests/directives/gameCell-test.js
+++ main-app/tests/directives/gameCell-test.js
@@ -2,8 +2,7 @@
     'use strict';
     describe('Testing game cell directive', function() {
         var compile,
-            rootScope,
-            sandbox;
+            rootScope;
 
         beforeEach(function () {
             module('Tombola.NoughtAndCrosses.gameboard');
@@ -12,23 +11,15 @@
                 compile = $compile;
                 rootScope = $rootScope;
             });
-
-            sandbox = sinon.sandbox.create();
         });
 
         it('Ensures game-cell directives works', function () {
-            var directiveElement =  '<game-cell square-number="4"></game-cell>';
+            var directiveElement = '<game-cell square-number="4"></game-cell>';
             var element = compile(directiveElement)(rootScope);
             rootScope.$digest();
             element[0].toString().should.be.equal('[object HTMLElement]');
             element.attr('square-number').should.be.equal('4');
             element[0].children.length.should.be.equal(1);
-            //element.attr('class').should.be.equal('gameCells player');
-            element.should.have.class('gameCells');
-        });
-
-        afterEach(function () {
-            sinon.sandbox.restore();
         });
     });
 })();
