9a8ce4aac6d8bca3ee8b804b4e504e61928ea739
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-21 13:54:13 +0100

    added more tests

--- main-app/tests/directives/gameCell-test.js
+++ main-app/tests/directives/gameCell-test.js
@@ -2,7 +2,10 @@
     'use strict';
     describe('Testing game cell directive', function() {
         var compile,
-            rootScope;
+            rootScope,
+            element,
+            directiveElement,
+            insideElement;
 
         beforeEach(function () {
             module('Tombola.NoughtAndCrosses.gameboard');
@@ -11,15 +14,28 @@
                 compile = $compile;
                 rootScope = $rootScope;
             });
+
+            directiveElement = '<game-cell square-number="4"></game-cell>';
+            element = compile(directiveElement)(rootScope);
+            rootScope.$digest();
+            insideElement = element.find('div');
         });
 
         it('Ensures game-cell directives works', function () {
-            var directiveElement = '<game-cell square-number="4"></game-cell>';
-            var element = compile(directiveElement)(rootScope);
-            rootScope.$digest();
             element[0].toString().should.be.equal('[object HTMLElement]');
-            element.attr('square-number').should.be.equal('4');
             element[0].children.length.should.be.equal(1);
+        });
+
+        it('Ensures game-cell directives has the correct class', function () {
+            insideElement.attr('class').should.be.equal('gameCells player');
+        });
+
+        it('Ensures game-cell directives has the correct square number attribute', function () {
+            element.attr('square-number').should.be.equal('4');
+        });
+
+        it('Ensures game-cell directives has the correct ng-click function', function () {
+            insideElement.attr('ng-click').should.be.equal('game.gameboardTapped(4)');
         });
     });
 })();
