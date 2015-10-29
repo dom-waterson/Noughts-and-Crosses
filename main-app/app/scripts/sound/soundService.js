1b315e447e3879be5772200196d0e549a49bee6c
Dominic Waterson <dominic.waterson@tombola.com>
2015-10-29 10:44:03 +0000

    got rid of previous installed jQuery, using document.getElmentById in service

--- main-app/app/scripts/sound/soundService.js
+++ main-app/app/scripts/sound/soundService.js
@@ -3,25 +3,18 @@
     angular.module('Tombola.NoughtAndCrosses.sound')
         .service('soundService', ['$timeout', function ($timeout){
             var me = this,
-                audio = '';
+                audio;
 
             var stopSound = function () {
-                audio.get(0).pause();
-                me.playing = false;
+                audio.pause();
             };
 
-            me.playing = false;
-
             me.playSound = function (start, length) {
-                if(!me.playing) {
-                    audio = angular.element('#sound');
-                    console.log(audio[0]);
-                    audio.prop('currentTime', start);
-                    audio.get(0).play();
-                    console.log("playing");
-                    me.playing = true;
-                    $timeout(stopSound, length, true);
-                }
+                audio = document.getElementById("sound");
+                audio.currentTime = start;
+                audio.play();
+                console.log(audio.duration);
+                $timeout(stopSound, length, true);
             };
         }]);
 })();
